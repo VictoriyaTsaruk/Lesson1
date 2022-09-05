2717df2f3e07e7d01779fcb680337476153e5459
Victoriya Tsaruk <tsaruk.work21212@gmail.com>
2022-09-05 18:12:21 +0300

    Program was added

---
+++ program.cs
@@ -0,0 +1,23 @@
+﻿{
+  Console.WriteLine("Calculate the sum");
+  int number1 = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine(number1 + number1);
+}
+
+{
+  Console.WriteLine("Calculate the subtraction");
+  int number2 = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine(number2 - number2);
+}
+
+{
+  Console.WriteLine("Calculate the division");
+  int number3 = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine(number3 / number3);
+}
+
+{
+  Console.WriteLine("Calculate the multiplication");
+  int number4 = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine(number4 * number4);
+}
